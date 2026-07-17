095538573fcbe2504b66c0638791b2da3943b316
lanmower <657315+lanmower@users.noreply.github.com>
2026-07-17 15:40:42 +0200

    chore: track spool-response-ready-protocol resolution and ack-sentinel follow-up

---
+++ scratch_serde_test/target/debug/deps/scratch_serde_test.d
@@ -0,0 +1,5 @@
+C:\dev\gm\scratch_serde_test\target\debug\deps\scratch_serde_test.d: src\main.rs
+
+C:\dev\gm\scratch_serde_test\target\debug\deps\scratch_serde_test.exe: src\main.rs
+
+src\main.rs:
---
+++ scratch_serde_test/target/debug/scratch_serde_test.d
@@ -0,0 +1,1 @@
+C:\dev\gm\scratch_serde_test\target\debug\scratch_serde_test.exe: C:\dev\gm\scratch_serde_test\src\main.rs
